28932d053314a7b8f9028a350a5fbcfad4cbf48c
Eleftheria <92717285+eboulaki@users.noreply.github.com>
2023-05-07 12:34:29 +0300

    ????

--- hangman.py
+++ hangman.py
@@ -5,40 +5,42 @@ def main():
     lives = 5
     words = ["koulis", "kolotoumpa", "backpack", "sfurodrepano"]
     word = random.choice(words)
-    print(word)
     choice = set(word)
     myWord = []
     guessedWord = []
     guessedLetters = []
+
     for letter in word:
         guessedWord.append("_")
     print(f"The word has {len(word)} letters")
     print(guessedWord)
     while lives > 0:
         myLetter = input("Your Letter: \n")
+
         try:
             is_valid(myLetter, guessedLetters)
         except ValueError as error:
             print(error)
             continue
-        if is_valid(myLetter, guessedLetters):
-            guessedLetters.append(myLetter)
-            if myLetter in choice:
-                print("Letter is in word: {0} time(s)".format(word.count(myLetter)))
-                for ind in find_indices(word, myLetter):
-                    guessedWord[ind] = myLetter
-                print(guessedWord)
-                myWord.append(myLetter)
-                if len(myWord) == len(choice):
-                    print("You won!")
-                    break
+
+        guessedLetters.append(myLetter)
+        if myLetter in choice:
+            print("Letter is in word: {0} time(s)".format(word.count(myLetter)))
+            for ind in find_indices(word, myLetter):
+                guessedWord[ind] = myLetter
+            print(guessedWord)
+            myWord.append(myLetter)
+            if len(myWord) == len(choice):
+                print("You won!")
+                break
+        else:
+            lives -= 1
+            if lives > 0:
+                print("Letter not in word")
+                print(f"Lives remaining: {lives} ")
             else:
-                lives -= 1
-                if lives > 0:
-                    print("Letter not in word")
-                    print(f"Lives remaining: {lives} ")
-                else:
-                    print("You lost")
+                print("You lost")
+
     print("Word was: " + word)
 
 
